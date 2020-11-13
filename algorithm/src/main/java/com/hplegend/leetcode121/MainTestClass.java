5cc1e549b8405db5b8bc44c7785af45d522bbd3a
hplegend <hp.he@qunar.com>
2020-11-14 01:02:19 +0800

    m

---
+++ algorithm/src/main/java/com/hplegend/leetcode121/MainTestClass.java
@@ -0,0 +1,16 @@
+package com.hplegend.leetcode121;
+
+/**
+ * @author hp.he
+ * @date 2020/11/14 0:31
+ */
+public class MainTestClass {
+
+    public static void main(String[] args) {
+        Leetcode121 leetcode121 = new Leetcode121();
+
+        int[] arr = new int[]{2,4,1};
+        System.out.println(leetcode121.maxProfit(arr));
+    }
+
+}
