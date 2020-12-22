c51e102d17aa8a13a2d85c34bef9be064b0f9291
hplegend <hp.he@qunar.com>
2020-12-22 09:42:11 +0800

    m

---
+++ algorithm/src/main/java/com/hplegend/tree/leetcode104/Leetcode104.java
@@ -0,0 +1,37 @@
+package com.hplegend.tree.leetcode104;
+
+import com.hplegend.base.TreeNode;
+
+/**
+ * @author hp.he
+ * @date 2020/12/22 9:34
+ */
+public class Leetcode104 {
+
+    /**
+     * *     int val;
+     * *     TreeNode left;
+     * *     TreeNode right;
+     * *     TreeNode() {}
+     * *     TreeNode(int val) { this.val = val; }
+     * *     TreeNode(int val, TreeNode left, TreeNode right) {
+     * *         this.val = val;
+     * *         this.left = left;
+     * *         this.right = right;
+     * *     }
+     */
+
+    // 递归
+    public int maxDepth(TreeNode root) {
+        if (null == root) {
+            return 0;
+        }
+
+        int legfDepth = maxDepth(root.getLeft());
+        int rightDepght = maxDepth(root.getRight());
+
+        return legfDepth > rightDepght ? legfDepth + 1 : rightDepght + 1;
+    }
+
+
+}
---
+++ algorithm/src/main/java/com/hplegend/tree/leetcode104/MainClass.java
@@ -0,0 +1,8 @@
+package com.hplegend.tree.leetcode104;
+
+/**
+ * @author hp.he
+ * @date 2020/12/22 9:34
+ */
+public class MainClass {
+}
