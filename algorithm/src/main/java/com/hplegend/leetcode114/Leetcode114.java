d46dad04c3cd82a15dd00bcda42726538fc8363f
hplegend <hp.he@qunar.com>
2021-01-03 12:08:45 +0800

    leetcode 114

---
+++ algorithm/src/main/java/com/hplegend/leetcode114/Leetcode114.java
@@ -0,0 +1,42 @@
+package com.hplegend.leetcode114;
+
+import com.hplegend.base.TreeNode;
+
+/**
+ * @author hp.he
+ * @date 2021/1/3 11:38
+ */
+public class Leetcode114 {
+
+    public void flatten(TreeNode root) {
+
+        // 递归
+        // 分别张开左右，然后把右侧连接到左侧
+
+        if (null == root) {
+            return;
+        }
+
+
+        flatten(root.left);
+        flatten(root.right);
+
+        //
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        // 替换root节点
+        root.left = null;
+        root.right = left;
+
+        // 拼接 right 到left的最后一个节点
+        TreeNode tmp = root.right;
+        while (tmp.right != null) {
+            tmp = tmp.right;
+        }
+
+        tmp.right = right;
+
+    }
+
+}
---
+++ algorithm/src/main/java/com/hplegend/leetcode114/MainClassTest.java
@@ -0,0 +1,8 @@
+package com.hplegend.leetcode114;
+
+/**
+ * @author hp.he
+ * @date 2021/1/3 11:39
+ */
+public class MainClassTest {
+}
